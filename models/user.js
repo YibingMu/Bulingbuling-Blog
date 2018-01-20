536639db8002863d96fb63403363f4d4d5e50b51
穆 一冰 <bokuyihi@bokumatombp.home>
2018-01-20 15:46:17 -0800

    Back-end register

--- models/user.js
+++ models/user.js
@@ -1,5 +1,114 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const usersSchema = require('../schemas/user');
+
+let emailLengthChecker = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        if (email.length < 5 || email.length > 30 ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validEmailChecker = (email) => {
+    if (!email) {
+        return false
+    } else {
+        const regExp = new RegExp(/^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/);
+        return regExp.test(email);
+    }
+};
+
+const emailValidators = [{
+    validator: emailLengthChecker,
+    message: 'E-mail must be at least 5 characters but no more than 30.'
+    },
+    {
+    validator: validEmailChecker,
+    message: 'It should be a valid email.'
+    }];
+
+let usernameLengthChecker = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        if (username.length < 3 || username.length > 15 ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validUsernameChecker = (username) => {
+    if (!username) {
+        return false
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+};
+
+const usernameValidators = [{
+    validator: usernameLengthChecker,
+    message: 'Username must be at least 3 characters but no more than 15.'
+},
+    {
+        validator: validUsernameChecker,
+        message: 'It should be a valid username.'
+    }];
+
+let passwordLengthChecker = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        if (password.length < 6 || password.length > 16 ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validPasswordChecker = (password) => {
+    if (!password) {
+        return false
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(password);
+    }
+};
+
+const passwordValidators = [{
+    validator: passwordLengthChecker,
+    message: 'Password must be at least 3 characters but no more than 15.'
+},
+    {
+        validator: validPasswordChecker,
+        message: 'It should be a valid password.'
+    }];
+
+const usersSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        validate: emailValidators
+    },
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+        validate: usernameValidators
+    },
+    password:{
+        type: String,
+        required: true,
+        validate: passwordValidators
+    }
+});
 
 module.exports = mongoose.model('User', usersSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,48 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.post('/register', (req, res) => {
-
+    if (!req.body.username) {
+        res.json({success: false, message: 'Username is required'});
+    } else {
+        if (!req.body.email) {
+            res.json({success: false, message: 'E-mail is required'});
+        } else {
+            if (!req.body.password) {
+                res.json({success: false, message: 'Password is required'});
+            } else {
+                let user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+                user.save((err) => {
+                    if (err) {
+                        if (err.code === 11000) {
+                            res.json({success: false, message: 'Username or E-mail has already exists'});
+                        } else{
+                            if (err.errors) {
+                                if (err.errors.email) {
+                                    res.json({success: false, message: err.errors.email.message});
+                                } else {
+                                    if (err.errors.username) {
+                                        res.json({success: false, message: err.errors.username.message});
+                                    } else {
+                                        if (err.errors.password) {
+                                            res.json({success: false, message: err.errors.password.message});
+                                        } else {
+                                            res.json({success: false, message: 'Could not save user. Error: ', err});
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        res.json({success: true, message: 'Registration Successfully'});
+                    }
+                });
+            }
+        }
+    }
 });
 
 module.exports = router;
